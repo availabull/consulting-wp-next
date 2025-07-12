2ef8cc0d5c04e90dfb11b216430b1d8b39fe12e5
Robert Fisher <robert@robertfisher.com>
2025-07-12 10:33:59 -0600

    remove errors in site health with code

---
+++ wordpress/web/app/mu-plugins/disable-file-mods.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Suppress the “some core files are not writable” Site‑Health test.
+ * The stack is immutable (Docker image); WP should never edit core.
+ */
+add_filter( 'site_status_tests', function ( $tests ) {
+    unset( $tests['async']['wordpress_filesystem'] );
+    return $tests;
+} );
