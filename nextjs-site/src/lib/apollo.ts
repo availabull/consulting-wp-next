b068e64d21252527604a949ae4d5b8100c1213c3
Robert Fisher <167127761+availabull@users.noreply.github.com>
2025-07-20 20:40:30 -0500

    Add default GraphQL endpoint

--- nextjs-site/src/lib/apollo.ts
+++ nextjs-site/src/lib/apollo.ts
@@ -1,8 +1,14 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const GRAPHQL_URL =
+  process.env.NEXT_PUBLIC_WPGRAPHQL_URL ??
+  'http://localhost:8080/graphql';
+
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_WPGRAPHQL_URL,
+  uri: GRAPHQL_URL,
   cache: new InMemoryCache(),
 });
+
+export { GRAPHQL_URL };
 
 export default client;
