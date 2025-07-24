96229f89e70051bcfa9a63107f498c22316177bd
Robert Fisher <robert@robertfisher.com>
2025-07-24 16:42:46 -0600

    added wp acf and other plugins auto into project

--- bin/wp-bootstrap.sh
+++ bin/wp-bootstrap.sh
@@ -14,3 +14,23 @@ docker compose exec wordpress bash -c '
   wp plugin activate wp-graphql --allow-root &&
   wp rewrite flush --hard --allow-root
 '
+
+
+wp core is-installed --allow-root || wp core install \
+  --url=https://wp.${DOMAIN} \
+  --title="Production Site" \
+  --admin_user=${WP_ADMIN_USER:-admin} \
+  --admin_password=${WP_ADMIN_PASS:-changeme} \
+  --admin_email=${WP_ADMIN_EMAIL:-you@example.com} \
+  --skip-email --allow-root &&
+
+
+wp plugin activate \
+  wp-graphql \
+  wp-graphql-menus \
+  wordpress-seo \
+  wp-graphql-yoast-seo \
+  --allow-root &&
+
+wp option update permalink_structure "/%postname%/" --allow-root &&
+wp rewrite flush --hard --allow-root
